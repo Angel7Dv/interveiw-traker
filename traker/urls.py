74303d00e42e2c3d4fa963c2d047b70d3e2d0dab
angel <angelriera1796@gmail.com>
2022-05-27 01:07:49 -0400

    urls conflict not fixed

--- traker/urls.py
+++ traker/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import dashboard, vacant, view_enterprise
+from .views import dashboard, vacant, view_enterprise, interview
 urlpatterns = [
     path('', dashboard, name="dashboard"), 
-    path('<str:slug_enterprise>/<str:vacant_slug>/', vacant, name="vacant"), #allow delete
-    path('<str:slug_enterprise>/', view_enterprise, name="view_enterprise"),
+
+    path(r'<str:interview_slug>/', interview, name="interview"), #allow delete
+    path(r'<str:slug_enterprise>/', view_enterprise, name="view_enterprise"),
+    path(r'<str:slug_enterprise>/<str:vacant_slug>/', vacant, name="vacant"), #allow delete
+
 
     # path('<str:enterprise>/<str:vacant>/<str:interview>', dashboard, name="dashboard"),
 
--- traker/views.py
+++ traker/views.py
@@ -7,6 +7,8 @@ from django.http import JsonResponse, HttpResponseRedirect
 # Create your views here.
 
 # http://127.0.0.1:8000/dashboard/
+
+
 def dashboard(request):
     vacants = Vacant.objects.all()
     add_vacant_form = VacantForm()
@@ -23,22 +25,27 @@ def dashboard(request):
     return render(request, 'traker/dashboard.html', ctx)
 
 # http://127.0.0.1:8000/dashboard/<vacant_slug>
+
+
 def vacant(request, slug_enterprise, vacant_slug):
     vacant = get_object_or_404(Vacant, slug=vacant_slug)
     vacant_form = VacantForm(instance=vacant)
-    # interview_form = InterviewForm()
+
+    # Delete
     if request.method == 'DELETE':
         vacant.delete()
         return JsonResponse({'success': "delete"}), redirect("index")
     if request.method == 'POST':
+        # PUT
         if 'ADD_ENTERPRISE' in request.POST:
-            query = request.POST.get('query', '')
+            query = request.POST.get('ADD_ENTERPRISE', '')
             vacant = Vacant.objects.get(slug=vacant_slug)
 
             enterprise = Enterprise.objects.get_or_create(name=query)
             vacant.enterprise = enterprise[0]
             vacant.save()
             return redirect("index")
+
         elif vacant_form.is_valid():
             vacant_form.save()
             return redirect("index")
@@ -59,10 +66,32 @@ def view_enterprise(request, slug_enterprise):  # set enterprise detail
         if form_enterprise.is_valid():
             form_enterprise.save()
             return redirect('view_enterprise', slug_enterprise)
-    ctx = {
-        'form_enterprise': form_enterprise,
-        'enterprise': enterprise,
-        'vacants': vacants
-    }
+    
+    else:
+        ctx = {
+            'form_enterprise': form_enterprise,
+            'enterprise': enterprise,
+            'vacants': vacants
+        }
 
-    return render(request, 'traker/enterprise_detail.html', ctx)
+        return render(request, 'traker/enterprise_detail.html', ctx)
+
+
+def interview(request, interview_slug):
+
+    # vacant = get_object_or_404(Vacant, slug=vacant_slug)
+    # interview = get_object_or_404(Interview, slug=interview_slug)
+
+    print("test1")
+    if request.method == 'POST':
+        print("test2")
+        if "ADD_INTERVIEW" in request.POST:
+            print("test3")
+
+            query = request.POST.get("query", "")
+
+            print(query, interview_slug)
+
+            # new_interview = Interview.objects.create()
+
+            return JsonResponse({"success": "success"})
